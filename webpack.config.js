1646810e02def17124b452c4d3f083fdab53529e
1Magz1 <46316981+1Magz1@users.noreply.github.com>
2023-05-13 17:38:06 +0600

    develop: add htmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
-const path = require('path')
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: "development",
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.join(__dirname, 'build'),
-  }
+    clean: true,
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    })
+  ]
 }
