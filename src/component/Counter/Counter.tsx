cc744004473fb92f88de545917d722e1ceb7c50d
1Magz1 <46316981+1Magz1@users.noreply.github.com>
2023-05-20 22:48:46 +0600

    develop: add counter component

---
+++ src/component/Counter/Counter.tsx
@@ -0,0 +1,20 @@
+import {useState} from 'react';
+import './Counter.scss';
+
+export const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  }
+  const decrement = () => {
+    setCount(count - 1);
+  }
+  return (
+    <div className="counter">
+      <h1>Current count is {count}</h1>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </div>
+  );
+};
