384d338250c2883fc6ad1f87eb1b581215e98d75
1Magz1 <46316981+1Magz1@users.noreply.github.com>
2023-05-24 22:30:27 +0600

    develop: add theme context

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,25 @@ import {
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {Suspense} from "react";
+import {useTheme} from "./context/theme/useTheme";
 
 const App = () => {
+  const {theme, toggleTheme} = useTheme();
+
   return (
-    <BrowserRouter>
-      <Link to="/">Main</Link>
-      <Link to="/about">About</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" Component={MainPageAsync}/>
-          <Route path="/about" Component={AboutPageAsync}/>
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+    <div className={`app ${theme}`}>
+      <BrowserRouter>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+        <Link to="/">Main</Link>
+        <Link to="/about">About</Link>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" Component={MainPageAsync}/>
+            <Route path="/about" Component={AboutPageAsync}/>
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </div>
   );
 };
 
---
+++ src/context/theme/ThemeProvider.tsx
@@ -0,0 +1,33 @@
+import {
+  ReactNode,
+  useMemo,
+  useState
+} from 'react';
+import {
+  LOCAL_STORAGE_THEME_KEY,
+  Theme,
+  ThemeContext
+} from './ThemeContext';
+
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+const ThemeProvider = (props: ThemeProviderProps) => {
+  const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+  const defaultProps = useMemo(() => ({
+    theme,
+    setTheme
+  }), [theme])
+
+  return (
+    <ThemeContext.Provider value={defaultProps}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
