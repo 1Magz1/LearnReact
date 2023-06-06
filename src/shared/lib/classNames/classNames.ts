16f8dce3304a3865f8716cebe8e763be5d3a0235
1Magz1 <46316981+1Magz1@users.noreply.github.com>
2023-06-06 16:18:01 +0600

    develop: fix className func

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,6 +1,6 @@
 type Mods = Record<string, boolean>
 
-export const classNames = (className: string, mods: Mods = {}, additional?: string[]) => {
+export const classNames = (className: string, mods: Mods = {}, additional: string[] = []) => {
   return [
     className,
     ...Object.entries(mods)
