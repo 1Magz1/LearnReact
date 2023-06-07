ab121585051e1c55c2a6ce61bf1e0307b9cd8150
1Magz1 <46316981+1Magz1@users.noreply.github.com>
2023-06-07 16:26:06 +0600

    develop: remove theme switcher to widgets

---
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -0,0 +1,29 @@
+import {classNames} from "shared/lib/classNames/classNames";
+
+import cls from "./ThemeSwitcher.module.scss";
+import {Theme, useTheme} from "app/providers/ThemeProvider";
+import {HTMLAttributes} from "react";
+
+import MoonIcon from "shared/assets/icons/full-moon.svg";
+import SunIcon from "shared/assets/icons/sun.svg";
+import {Button} from "shared/ui/Button";
+import {ThemeButton} from "shared/ui/Button/ui/Button";
+
+interface ThemeSwitcherProps extends HTMLAttributes<HTMLButtonElement>{
+  className?: string;
+}
+
+export const ThemeSwitcher = (props: ThemeSwitcherProps) => {
+  const {className} = props;
+  const {theme, toggleTheme} = useTheme();
+
+  return (
+    <Button
+      theme={ThemeButton.CLEAR}
+      onClick={toggleTheme}
+      className={classNames(cls.themeSwitcher, {}, [className])}
+    >
+      {theme === Theme.DARK ? <MoonIcon /> : <SunIcon/>}
+    </Button>
+  );
+};
