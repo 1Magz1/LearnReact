912b2c9341a627423fe384424a0afd215bacfa44
1Magz1 <46316981+1Magz1@users.noreply.github.com>
2023-06-11 18:13:57 +0600

    develop: add i18n into App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,5 @@
 import './styles/index.scss'
+import {Suspense} from "react";
 import {BrowserRouter} from "react-router-dom";
 
 import {classNames} from "shared/lib/classNames/classNames";
@@ -7,15 +8,19 @@ import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/Router";
 import {Navbar} from "widgets/Navbar";
 
+import "shared/config/i18n/i18n";
+
 const App = () => {
   const {theme} = useTheme();
 
   return (
     <div className={classNames('app', {"class1": true}, [theme])}>
-      <BrowserRouter>
-        <Navbar/>
-        <AppRouter/>
-      </BrowserRouter>
+      <Suspense fallback="">
+        <BrowserRouter>
+          <Navbar/>
+          <AppRouter/>
+        </BrowserRouter>
+      </Suspense>
     </div>
   );
 };
