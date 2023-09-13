a45c155427d358a13188d4ad193a5cb689a5cc25
1Magz1 <46316981+1Magz1@users.noreply.github.com>
2023-09-13 18:39:10 +0600

    develop: add error boundary

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -4,12 +4,6 @@ import { useEffect } from 'react';
 function MainPage() {
   const { t, i18n } = useTranslation('main');
 
-  useEffect(() => {
-    if (Math.random() > 0.5) {
-      throw new Error();
-    }
-  }, []);
-
   return (
     <div>
       <h1>{t('title')}</h1>
