b889e726568a07ee9bb45f06aadebdb7f6c82ec6
1Magz1 <46316981+1Magz1@users.noreply.github.com>
2023-09-16 16:37:22 +0600

    develop: add mock for svg

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -4,6 +4,7 @@
  */
 
 import type { Config } from 'jest';
+import path from 'path';
 
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
@@ -92,6 +93,7 @@ const config: Config = {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     '\\.(s?css)$': 'identity-obj-proxy',
+    '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
