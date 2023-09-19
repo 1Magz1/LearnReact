efadc8abe954a9df8a22f398311d2a4f493f2bff
1Magz1 <46316981+1Magz1@users.noreply.github.com>
2023-09-19 16:41:25 +0600

    develop: add app link storybook

---
+++ src/shared/ui/AppLink/ui/AppLink.stories.ts
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { themeDecorator } from 'shared/config/Storybook/decorators/themeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { DefaultDark } from 'shared/ui/Button/ui/Button.stories';
+import { AppLink, AppLinkThemes } from './AppLink';
+
+const meta = {
+  title: 'shared/AppLink',
+  component: AppLink,
+} satisfies Meta<typeof AppLink>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    children: 'Text',
+    className: AppLinkThemes.PRIMARY,
+    to: '/',
+  },
+};
+
+export const PrimaryDark: Story = {
+  args: {
+    children: 'Text',
+    className: AppLinkThemes.PRIMARY,
+    to: '/',
+  },
+};
+PrimaryDark.decorators = [
+  themeDecorator(Theme.DARK),
+];
+
+export const Secondary: Story = {
+  args: {
+    children: 'Text',
+    className: AppLinkThemes.SECONDARY,
+    to: '/',
+  },
+};
+
+export const SecondaryDark: Story = {
+  args: {
+    children: 'Text',
+    className: AppLinkThemes.SECONDARY,
+    to: '/',
+  },
+};
+SecondaryDark.decorators = [
+  themeDecorator(Theme.DARK),
+];
