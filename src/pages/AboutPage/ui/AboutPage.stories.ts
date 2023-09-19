948e0b668735bb87b788f75232da8c7473dcf024
1Magz1 <46316981+1Magz1@users.noreply.github.com>
2023-09-19 16:50:45 +0600

    develop: add pages storybook

---
+++ src/pages/AboutPage/ui/AboutPage.stories.ts
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { themeDecorator } from 'shared/config/Storybook/decorators/themeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import AboutPage from './AboutPage';
+
+const meta = {
+  title: 'pages/AboutPage',
+  component: AboutPage,
+} satisfies Meta<typeof AboutPage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+  },
+};
+
+export const PrimaryDark: Story = {
+  args: {
+  },
+};
+PrimaryDark.decorators = [
+  themeDecorator(Theme.DARK),
+];
