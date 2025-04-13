98b071fc051556f9096ccbd87ff29df51d39446b
Maksim Chipurnov <46316981+1Magz1@users.noreply.github.com>
2025-04-13 19:15:09 +0700

    develop: add custom input component

---
+++ src/shared/ui/Input/ui/Input.tsx
@@ -0,0 +1,32 @@
+import { ChangeEvent, HTMLAttributes } from 'react';
+import cls from './Input.module.scss';
+
+interface InputProps extends Omit<HTMLAttributes<HTMLInputElement>, 'onChange'> {
+  value: string;
+  onChange?: (value: string) => void;
+  label?: string;
+}
+
+const Input = (props: InputProps) => {
+  const {
+    value, onChange, label, ...otherProps
+  } = props;
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onChange?.(event.target.value);
+  };
+
+  return (
+    <div>
+      {label && <span className={cls.Label}>{label}</span>}
+      <input
+        className={cls.Input}
+        value={value}
+        onChange={handleChange}
+        {...otherProps}
+      />
+    </div>
+  );
+};
+
+export default Input;
