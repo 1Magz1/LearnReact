803552d1da78dbb3b421695b28375c98855d9075
Maksim Chipurnov <46316981+1Magz1@users.noreply.github.com>
2025-04-14 14:15:54 +0700

    develop: update storybook theme decorator

--- src/shared/config/Storybook/decorators/themeDecorator.tsx
+++ src/shared/config/Storybook/decorators/themeDecorator.tsx
@@ -2,9 +2,9 @@ import { Theme } from 'app/providers/ThemeProvider';
 import { StoryFn } from '@storybook/react';
 
 export const themeDecorator = (theme: Theme) => function (Story: StoryFn) {
+  document.body.className = theme;
+
   return (
-    <div className={`app ${theme}`}>
-      <Story />
-    </div>
+    <Story />
   );
 };
