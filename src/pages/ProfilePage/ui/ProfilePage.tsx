542d96f46174311e10b2aa895d0edb0eefd5f9b6
Maksim Chipurnov <46316981+1Magz1@users.noreply.github.com>
2025-04-18 14:23:48 +0700

    develop: update profile page

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,8 +1,52 @@
+import { useTranslation } from 'react-i18next';
+import DynamicReducerLoader, { ReducerObject } from 'shared/lib/components/DynamicReducerLoader/DynamicReducerLoader';
+import {
+  profileReducer, getProfileData, ProfileCard, fetchProfileData,
+} from 'features/UserProfile';
+import { useCallback, useEffect, useState } from 'react';
+import { useAppDispatch } from 'app/providers/StoreProvider';
+import { useSelector } from 'react-redux';
+import { Loader } from 'widgets/Loader';
+import Text from 'widgets/Text/Text';
+import cls from './ProfilePage.module.scss';
+
+const reducerList: ReducerObject[] = [
+  {
+    name: 'profile',
+    reducer: profileReducer,
+  },
+];
+
 function ProfilePage() {
+  const { t } = useTranslation('profile');
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useAppDispatch();
+  const profileData = useSelector(getProfileData);
+
+  const fetchProfile = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      await dispatch(fetchProfileData());
+    } catch (error) {
+      console.error(error);
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    fetchProfile().finally(() => setIsLoading(false));
+  }, []);
+
   return (
-    <div>
-      <span>Profile Page</span>
-    </div>
+    <DynamicReducerLoader reducerList={reducerList}>
+      <Text variant="h1">
+        {t('title')}
+      </Text>
+      {!isLoading ? (
+        <ProfileCard profile={profileData} className={cls.card} />
+      ) : (
+        <Loader />
+      )}
+    </DynamicReducerLoader>
   );
 }
 
