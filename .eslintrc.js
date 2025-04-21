80afdec33e7fcdca63b3ba4de96794b7bd3d391a
Maksim Chipurnov <46316981+1Magz1@users.noreply.github.com>
2025-04-21 21:36:06 +0700

    develop: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,7 @@ module.exports = {
     "max-len": ["error", { ignoreComments: true, code: 120 }],
     'jsx-a11y/no-static-element-interactions': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
-    'no-param-reassign': 'off'
+    'no-param-reassign': 'off',
+    'react/button-has-type': 'off'
   }
 }
