88aaa99a87dceb7af99c06877f4e0678534fe41b
Maksim Chipurnov <46316981+1Magz1@users.noreply.github.com>
2025-04-21 18:24:33 +0700

    develop: add use local storage hook

---
+++ src/shared/hooks/useLocalStorage.ts
@@ -0,0 +1,58 @@
+import { useState, useEffect, useCallback } from 'react';
+
+type SetValue<T> = (value: T) => void;
+type RemoveValue = () => void;
+
+export const STORAGE_EVENT = 'local-storage';
+
+export function useLocalStorage<T>(
+  key: string,
+  initialValue: T,
+): [T, SetValue<T>, RemoveValue] {
+  const readValue = useCallback((): T => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.warn(`Error reading localStorage key "${key}":`, error);
+      return initialValue;
+    }
+  }, [key, initialValue]);
+
+  const [storedValue, setStoredValue] = useState<T>(readValue);
+
+  const setValue: SetValue<T> = useCallback(
+    (value) => {
+      try {
+        localStorage.setItem(key, JSON.stringify(value));
+        setStoredValue(value);
+
+        window.dispatchEvent(new Event(STORAGE_EVENT));
+      } catch (error) {
+        console.warn(`Error setting localStorage key "${key}":`, error);
+      }
+    },
+    [key, storedValue],
+  );
+
+  const removeValue: RemoveValue = useCallback(() => {
+    try {
+      localStorage.removeItem(key);
+      setStoredValue(initialValue);
+      window.dispatchEvent(new Event(STORAGE_EVENT));
+    } catch (error) {
+      console.warn(`Error removing localStorage key "${key}":`, error);
+    }
+  }, [key, initialValue]);
+
+  useEffect(() => {
+    const handleLocalChange = () => {
+      setStoredValue(readValue());
+    };
+
+    window.addEventListener(STORAGE_EVENT, handleLocalChange);
+    return () => window.removeEventListener(STORAGE_EVENT, handleLocalChange);
+  }, [readValue]);
+
+  return [storedValue, setValue, removeValue];
+}
