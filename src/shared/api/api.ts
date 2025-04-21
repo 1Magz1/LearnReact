7159c7865ff30955019556ca18b7f794095143ab
Maksim Chipurnov <46316981+1Magz1@users.noreply.github.com>
2025-04-21 18:35:40 +0700

    develop: fix

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,7 +3,14 @@ import { LOCAL_STORAGE_USERNAME_KEY } from 'shared/constants';
 
 export const $api = ky.extend({
   prefixUrl: 'http://localhost:8000/',
-  headers: {
-    Authorization: localStorage.getItem(LOCAL_STORAGE_USERNAME_KEY) || '',
+  hooks: {
+    beforeRequest: [
+      (request) => {
+        const token = localStorage.getItem(LOCAL_STORAGE_USERNAME_KEY);
+        if (token) {
+          request.headers.set('Authorization', token);
+        }
+      },
+    ],
   },
 });
