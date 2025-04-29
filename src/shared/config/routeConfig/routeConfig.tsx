8dd9e77c74a82160a4c387369eddae0174a58644
Maksim Chipurnov <46316981+1Magz1@users.noreply.github.com>
2025-04-29 20:57:43 +0700

    develop: update route config

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -3,11 +3,15 @@ import { AboutPage } from 'pages/AboutPage';
 import { NotFoundPage } from 'pages/NotFoundPage';
 import { ProfilePage } from 'pages/ProfilePage';
 import { ReactNode } from 'react';
+import { ArticlesPage } from 'pages/ArticlesPage';
+import { ArticleDetailsPage } from 'pages/ArticleDitailsPage';
 
 export enum appRoutes {
   MAIN = 'main',
   ABOUT = 'about',
   PROFILE = 'profile',
+  ARTICLES = 'articles',
+  ARTICLE_DETAILS = 'article_details',
   NOT_FOUND = 'not_found',
 }
 
@@ -21,6 +25,8 @@ export const routePath: Record<appRoutes, string> = {
   [appRoutes.MAIN]: '/',
   [appRoutes.ABOUT]: '/about',
   [appRoutes.PROFILE]: '/profile',
+  [appRoutes.ARTICLES]: '/articles',
+  [appRoutes.ARTICLE_DETAILS]: '/articles/',
   [appRoutes.NOT_FOUND]: '*',
 };
 
@@ -36,6 +42,16 @@ export const routeConfig: Record<appRoutes, AppRouteProps> = {
   [appRoutes.PROFILE]: {
     path: routePath[appRoutes.PROFILE],
     element: <ProfilePage />,
+    authOnly: true,
+  },
+  [appRoutes.ARTICLES]: {
+    path: routePath[appRoutes.ARTICLES],
+    element: <ArticlesPage />,
+    authOnly: true,
+  },
+  [appRoutes.ARTICLE_DETAILS]: {
+    path: `${routePath[appRoutes.ARTICLE_DETAILS]}:id`,
+    element: <ArticleDetailsPage />,
     authOnly: true,
   },
   [appRoutes.NOT_FOUND]: {
