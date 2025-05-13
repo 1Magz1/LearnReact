b8169f74943dbdec3cfa82f6ffcd657e67143081
Maksim Chipurnov <46316981+1Magz1@users.noreply.github.com>
2025-05-13 18:50:59 +0700

    develop: add ImageBlock

---
+++ src/entities/Article/ui/ImageBlock/ImageBlock.tsx
@@ -0,0 +1,19 @@
+import { Text } from 'shared/ui/Text';
+import { ImageBlockType } from '../../model/schema/articleSchema';
+import cls from './ImageBlock.module.scss';
+
+interface ImageBlockProps {
+  block: ImageBlockType;
+}
+
+const ImageBlock = (props: ImageBlockProps) => {
+  const { block } = props;
+  return (
+    <div className={cls.block}>
+      <img src={block.src} alt={block.title} className={cls.image} />
+      {block.title && <Text data-testId="image-caption" variant="span">{block.title}</Text>}
+    </div>
+  );
+};
+
+export default ImageBlock;
